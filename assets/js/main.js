0fc8d9f3d12aa2ae2cc7b5b7ff476f8c53c9b176
Shivam Gupta <shivamgupta@Shivams-MacBook-Air.local>
2023-08-25 16:12:45 +0530

    sorted bug 10

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,24 +9,38 @@ input.oninput = function () {
 }
 
 
+var index = 0;
 
-// // Get all input fields
-// const inputFields = document.querySelectorAll('.user-box input');
+show_slide = (i) => {
+  //increment/decrement slide index
+  index += i;
 
-// // Add event listeners to each input field
-// inputFields.forEach(input => {
-//     input.addEventListener('focus', () => {
-//         input.parentNode.classList.add('active');
-//     });
+  //grab all the images
+  var images = document.getElementsByClassName("image");
+  //grab all the dots
+  var dots = document.getElementsByClassName("dot");
 
-//     input.addEventListener('blur', () => {
-//         if (input.value === '') {
-//             input.parentNode.classList.remove('active');
-//         }
-//     });
+  // hide all the images
+  for (i = 0; i < images.length; i++) 
+    images[i].style.display = "none";
+  
+  // remove the active class from the dot
+  for (i = 0; i < dots.length; i++) 
+    dots[i].className = dots[i].className.replace(" active", "");
+  
+  // if index is greater than the amount of images (set it to zero)
+  if (index > images.length - 1) 
+    index = 0 ;
+  
+  // if index is less than zero (set it to the length of images)
+  if (index < 0)
+    index = images.length - 1;
 
-//     // Check input values on page load
-//     if (input.value !== '') {
-//         input.parentNode.classList.add('active');
-//     }
-// });
+  // only display the image that's next or previous
+  images[index].style.display = "block";
+  // only make the current dot active
+  dots[index].className += " active";
+
+}
+
+window.addEventListener("onload", show_slide(index));
